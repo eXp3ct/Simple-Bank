1665d098ed0e4ea22272644bb19b57694c7fb491
Рома Веселов <MWoShka@outlook.com>
2023-10-02 00:15:36 +0300

    viper env configuration

--- main.go
+++ main.go
@@ -6,17 +6,18 @@ import (
 
 	"github.com/eXpect/api"
 	db "github.com/eXpect/db/sqlc"
+	"github.com/eXpect/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:root@localhost:10101/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to db: ", err)
@@ -25,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("Cannot start server", err)
